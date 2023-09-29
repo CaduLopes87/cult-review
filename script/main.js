061526258609dfc2d6840a36a3111b11c2112e29
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-09-28 23:27:50 -0300

    organização dos módulos e incremento da barra de gêneros

--- script/main.js
+++ script/main.js
@@ -1,19 +1,9 @@
-import apiUrl from "./API";
+import { apiUrl } from "./API.js";
+import { obterDadosDaApi } from "./requisicoes.js";
 
 const filmeCelula = document.getElementById('filme-container');
 
-async function obterDadosDaApi(url){
-    const response = await fetch(url);
 
-    // console.log(response);
-
-    const data = await response.json()
-    // console.log(data);
-
-    return data;
-}
-
-// const filme = obterDadosDaApi(apiUrl);
 obterDadosDaApi(apiUrl).then(filme => {
     filmeCelula.innerHTML += `<div class="filme">${filme.title}</div>`
 });
