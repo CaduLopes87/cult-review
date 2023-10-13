2597666ff2d36deaf59f3a28be43ee06bf474a6c
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-10-13 16:16:06 -0300

    gitignore changes

---
+++ script/API.js
@@ -0,0 +1,11 @@
+export const apiUrl = 'https://api.themoviedb.org/3/movie/1008042?api_key=a8ef96a8478ba03d3138ded107eb1def&language=pt-br';
+
+export function obterURL(param) {
+    const novaURL = `https://api.themoviedb.org/3/movie/${param}?api_key=a8ef96a8478ba03d3138ded107eb1def&language=pt-br`;
+    return novaURL;
+} 
+
+export function obterURLDoPoster(param){
+    const novaURLPoster = `https://image.tmdb.org/t/p/w500/${param}`
+    return novaURLPoster;
+}
