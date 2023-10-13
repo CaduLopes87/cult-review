42cc577c0541d54f9f9d2eaf80b6bf8a7a48c392
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-10-13 20:20:00 -0300

    creating environment variable

--- script/API.js
+++ script/API.js
@@ -1,5 +1,6 @@
-const apiKey = process.env.API_KEY;
-console.log("API_KEY:", apiKey);
+// const apiKey = process.env.API_KEY;
+
+const apiKey = a8ef96a8478ba03d3138ded107eb1def;
 
 export const apiUrl = `https://api.themoviedb.org/3/movie/1008042?apiKey=${apiKey}&language=pt-br`;
 
